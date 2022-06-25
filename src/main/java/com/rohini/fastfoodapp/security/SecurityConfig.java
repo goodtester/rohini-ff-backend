6d25e78b103ed0e84b6c5ca04b5722051825458a
Rohini <Rohini@Rohini>
2022-06-24 21:15:49 -0400

    Initial

--- src/main/java/com/rohini/fastfoodapp/security/SecurityConfig.java
+++ src/main/java/com/rohini/fastfoodapp/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization"));
 		cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		cc.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
-		cc.addAllowedOrigin("http://localhost:4200");
+		cc.addAllowedOrigin("http://localhost:4200, http://rohini-mcburger.s3-website-us-east-1.amazonaws.com");
 		cc.setMaxAge(Duration.ZERO);
 		cc.setAllowCredentials(Boolean.TRUE);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
